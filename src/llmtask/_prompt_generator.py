046418cedf83b5c7fdc85c086ddbfadf8bbbd142
Slightwind <slightwindsec@gmail.com>
2023-12-04 23:06:04 +0800

    dev load csv file

--- src/llmtask/_prompt_generator.py
+++ src/llmtask/_prompt_generator.py
@@ -32,7 +32,6 @@ class TaskGenerator:
         # sub directory of the dataset, 'dev', 'val', 'test'
         self.sub_dir = "val" if kwargs.get('sub_dir') is None else kwargs.get('sub_dir')
         
-        # TODO: Read start position from cache file
         self.start = 0 if kwargs.get('start') is None else kwargs.get('start')
         self.end = kwargs.get('end')
         self.subject_names = SUBJECT_NAMES_MAP[self.dataset]
@@ -41,13 +40,35 @@ class TaskGenerator:
         if self.log_dir and not os.path.exists(self.log_dir):
             os.mkdir(self.log_dir)
         
+        self._load_cache()
+        
         self.subject_ptr = 0
         self.problem_ptr = 0
+        
         self.total_task_num = self._get_total_task_num()
+        
+        self.prompt_prefix = None
+        
+        self.subject_train_df = self._load_subject_train_df()
+        self.subject_test_df = self._load_subject_test_df()
         
         self.pbar = None
         if kwargs.get('pbar') is not False:
             self.pbar = tqdm.tqdm(total=self.total_task_num, initial=self.start)
+
+
+    def _load_cache(self):
+        if not self.log_dir:
+            return
+        cache_file = os.path.join(self.log_dir, f"{self.dataset}_answers.csv")
+        if not os.path.exists(cache_file):
+            return
+        with open(cache_file, "r") as f:
+            c = f.read().strip().split("\n")
+        if not c or len(c) <= 1:
+            return
+        self.start = int(c[-1].split(", ")[0])
+
 
     def _read_csv(self, csv_path, dev=False):
         if self.dataset == 'ceval':
@@ -56,6 +77,27 @@ class TaskGenerator:
         elif self.dataset == 'mmlu':
             df = pd.read_csv(csv_path, names=['question', 'A', 'B', 'C', 'D', 'answer'])
         return df
+
+
+    def _load_subject_train_df(self, train=False):
+        _sub_dir = "dev" if train else self.sub_dir
+        subject_name = self.subject_names[self.subject_ptr]
+        _tasks_dir = os.path.join(self.dataset_dir, _sub_dir)
+        _dev_csv_path = os.path.join(_tasks_dir, f"{subject_name}_{_sub_dir}.csv")
+        return self._read_csv(csv_path, train)
+
+
+    def _load_subject_test_df(self):
+        subject_name = self.subject_names[self.subject_ptr]
+        _tasks_dir = os.path.join(self.dataset_dir, self.sub_dir)
+        _csv_path = os.path.join(_tasks_dir, f"{subject_name}_{self.sub_dir}.csv")
+        return self._read_csv(csv_path)
+    
+    
+    def _construct_prompt_prefix(self):
+        '''Construct a N-Shot prompt prefix from the `dev` subdirectory'''
+        pass
+        
     
     def _get_total_task_num(self):
         '''returns the total number of tasks'''
